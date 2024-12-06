4002df2d802047511ad11230d3806c4bcce69be8
Murtaza Haidary <LE1983326@crc-lennox.qc.ca>
2024-12-06 01:47:26 -0500

    creating a new repo for my part

---
+++ app/src/main/java/com/example/finaleprojectbudget/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.budgetapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.finaleprojectbudget.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
